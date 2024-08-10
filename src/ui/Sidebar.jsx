1f0cf9f5712353cbdee4468a6b3f7141f2aa6c14
Ahmed Adel <ahmed.adel.dev@hotmail.com>
2024-08-10 08:04:59 +0300

    Updated layout

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,13 +1,23 @@
 import styled from "styled-components";
+import Logo from "./Logo";
+import MainNav from "./MainNav";
 
 const StyledSidebar = styled.aside`
   grid-row: 1/ -1;
   background-color: var(--color-gray-0);
   border-right: 1px solid var(--color-gray-100);
   padding: 3.2rem 2.4rem;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `;
 function Sidebar() {
-  return <StyledSidebar>Sidebar</StyledSidebar>;
+  return (
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+    </StyledSidebar>
+  );
 }
 
 export default Sidebar;
