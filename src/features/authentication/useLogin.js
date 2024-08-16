2023ad36a6d0f4fae9d7ce9cb47ce15b337a408d
Ahmed Adel <ahmed.adel.dev@hotmail.com>
2024-08-16 18:40:58 +0300

    Added login

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,19 @@
+import { useMutation } from "@tanstack/react-query";
+import { login as loginApi } from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+export default function () {
+  const navigate = useNavigate();
+  const { mutate: login, isPending } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
+    onSuccess: () => {
+      navigate("/dashboard");
+    },
+    onError: (err) => {
+      console.log("Error", err);
+      toast.error("Incorrect email or password");
+    },
+  });
+  return { login, isPending };
+}
