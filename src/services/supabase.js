e87bbbe2b4c6b0705acca93bd4c8e2d809ed8ab2
ahmed adel <ahmed.adel.dev@hotmail.com>
2025-01-14 19:04:51 +0200

    restored the database

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,4 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://ipbpsxhqaiwzifejhybb.supabase.co";
+export const supabaseUrl = "https://pzqtyaoyekahgjwsrnzh.supabase.co";
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 export default createClient(supabaseUrl, supabaseKey);
